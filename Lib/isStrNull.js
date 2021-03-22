c97d4aa1d050739a636cec8d5b14f725c5399347
SergeyMC9730 <bdsservers@yandex.ru>
2021-03-22 10:35:31 +0800

    New branch

--- Lib/isStrNull.js
+++
@@ -1,8 +0,0 @@
-module.exports = (str) => {
-	// get string
-	var a = "" + str
-	// is string equals to "null", "undefined" or "NaN"
-	if(a === "undefined" || a === "null" || a === "NaN") {
-		return true
-	} else return false
-}
